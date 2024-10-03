16e3a84d58ad4ef7e755e509f1581defccb4c487
murilovess <161042765+murilovess@users.noreply.github.com>
2024-10-03 14:19:10 +0000

    arqui_2

--- arquivo_2.py
+++ arquivo_2.py
@@ -5,3 +5,11 @@ print("Muriloves")
 
 
 print("MP: Minha pica")
+#resolução de conflito
+#git rebase
+#apague as linhas indesejadas 
+
+#git add .
+#git commit - m "mensagens"
+#git pull 
+#git push
