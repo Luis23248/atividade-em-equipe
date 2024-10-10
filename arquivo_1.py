6e6b494ada443b9b61332aada2b37428125808f4
murilovess <161042765+murilovess@users.noreply.github.com>
2024-10-10 12:22:00 +0000

    projeto

--- arquivo_1.py
+++
@@ -1,1 +0,0 @@
-print("LUIS PEDRO")
---
+++ projeto/abstract/funcionario.py
@@ -0,0 +1,10 @@
+from projeto.endereco import Endereco
+from abc import ABC, abstractmethod
+
+class Funcionario(ABC):
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco) -> None:
+        self.nome = nome
+        self.telefone - telefone
+        self.email = email
+        self.endereco = endereco
+        
